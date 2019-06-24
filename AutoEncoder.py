b3a59cc67029b3e5562f13f0bffc886b460c597c
ZhiLongChen <tsche107389@gmail.com>
2019-06-24 21:15:25 +0800

    更新code

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -2,72 +2,110 @@ import numpy as np
 from sklearn.datasets import kddcup99
 from keras.layers import Input, Dense
 from keras.models import Model, Sequential
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import EarlyStopping
 from DataPreprocess import preprocess_data, preprocess_target
-
-label_list = ['normal.', 'buffer_overflow.', 'loadmodule.', 'perl.', 'neptune.', 'smurf.',
-              'guess_passwd.', 'pod.', 'teardrop.', 'portsweep.', 'ipsweep.', 'land.', 'ftp_write.',
-              'back.', 'imap.', 'satan.', 'phf.', 'nmap.', 'multihop.', 'warezmaster.', 'warezclient.',
-              'spy.', 'rootkit.']
-
+import csv
+import datetime
 
 if __name__ == '__main__':
+    with open('Autoencoder_{}_{}_{}_{}.csv'.format(datetime.datetime.now().date(), 
+                                                   datetime.datetime.now().hour, 
+                                                   datetime.datetime.now().minute, 
+                                                   datetime.datetime.now().second), 'w', newline='') as csv_file:
+        header = ['Agnostic Class',
+                  'Number of Agnostic Class',
+                  'Number of Aware Class',
+                  'Number of Training Data',
+                  'Number of Testing Data',
+                  'Training Data Average Loss',
+                  'Testing Data Average Loss',
+                  'Aware Class Average Loss',
+                  'Agnostic Class Average Loss']
+        writer = csv.writer(csv_file)
+        writer.writerow(header)
 
-    dataset = kddcup99.fetch_kddcup99()
-    data = dataset.data
-    labels = dataset.target
+        label_list = [b'normal.', b'buffer_overflow.', b'loadmodule.', b'perl.', b'neptune.', b'smurf.',
+                      b'guess_passwd.', b'pod.', b'teardrop.', b'portsweep.', b'ipsweep.', b'land.', b'ftp_write.',
+                      b'back.', b'imap.', b'satan.', b'phf.', b'nmap.', b'multihop.', b'warezmaster.', b'warezclient.',
+                      b'spy.', b'rootkit.']
 
-    # 數據預處理
-    data = preprocess_data(data)
+        dataset = kddcup99.fetch_kddcup99()
+        data = dataset.data
+        labels = dataset.target
 
-    # parameter
-    DATA_LENGTH = data.shape[0]
-    INPUT_SHAPE = data.shape[1]
-    NEW_CLASS = b'smurf.'
-    CODES = 2
-    # parameter
+        # 數據預處理
+        data = preprocess_data(data)
 
-    input_data_test_set = list()
-    input_data_train_set = list()
+        # parameter
+        DATA_LENGTH = data.shape[0]
+        INPUT_SHAPE = data.shape[1]
+        CODES = 2
+        # parameter
 
-    for index, each in enumerate(labels[:DATA_LENGTH]):
-        if each == NEW_CLASS:
-            input_data_test_set.append(data[index])
-        else:
-            input_data_train_set.append(data[index])
+        # model configure
+        input_layer = Input(shape=(INPUT_SHAPE,))
+        encode_layer = Dense(units=20, activation='tanh')(input_layer)
+        code_layer = Dense(units=CODES)(encode_layer)
+        decode_layer = Dense(units=20, activation='tanh')(code_layer)
+        output_layer = Dense(units=INPUT_SHAPE)(decode_layer)
 
-    input_data_test_set = np.array(input_data_test_set)
-    input_data_train_set = np.array(input_data_train_set)
+        autoencoder = Model(input_layer, output_layer)
 
-    '''
-    print('New class: {}'.format(count))
-    print('Old class: {}'.format(count_))
-    print('Total: {}'.format(DATA_LENGTH))
+        autoencoder.summary()
 
-    # input_data_train_set = data[0:300000]
-    # input_data_test_set = data[300000::]
-    '''
-    
-    input_layer = Input(shape=(INPUT_SHAPE,))
-    encode_layer = Dense(units=20, activation='tanh')(input_layer)
-    code_layer = Dense(units=CODES)(encode_layer)
-    decode_layer = Dense(units=20, activation='tanh')(code_layer)
-    output_layer = Dense(units=INPUT_SHAPE)(decode_layer)
+        monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1, mode='auto')
+        autoencoder.compile(loss='mse', optimizer='adam')
+        # model configure
 
-    autoencoder = Model(input_layer, output_layer)
-    
-    '''
-    autoencoder = Sequential()
-    autoencoder.add(Dense(units=20, input_shape=(INPUT_SHAPE,), activation='tanh'))
-    autoencoder.add(Dense(units=2, activation='tanh'))
-    autoencoder.add(Dense(units=20, activation='tanh'))
-    autoencoder.add(Dense(units=41))
-    '''
-    autoencoder.summary()
+        for new_class in label_list:
+            input_data_agnostic_set = list()
+            input_data_aware_set = list()
+            input_data_test_set = list()
+            input_data_train_set = list()
+            
+            for index, each in enumerate(labels[:DATA_LENGTH]):
+                if each == new_class:
+                    input_data_agnostic_set.append(data[index])
+                else:
+                    input_data_aware_set.append(data[index])
 
-    autoencoder.compile(loss='mse', optimizer='adam')
-    autoencoder.fit(input_data_train_set, input_data_train_set, verbose=1, epochs=50)
-    autoencoder.save('autoencoder.h5')
+            input_data_agnostic_set = np.array(input_data_agnostic_set)
+            input_data_test_set = np.array(input_data_aware_set[int(len(input_data_aware_set)*4/5):])
+            input_data_train_set = np.array(input_data_aware_set[:int(len(input_data_aware_set)*4/5)])
+            input_data_aware_set = np.array(input_data_aware_set)
 
-    loss = autoencoder.evaluate(input_data_test_set, input_data_test_set)
-    print("Loss:{}.".format(loss))
+            autoencoder.fit(input_data_train_set, input_data_train_set, 
+                            validation_data=(input_data_test_set, input_data_test_set), 
+                            verbose=0, epochs=50, callbacks=[monitor,])
+
+            new_class = str(new_class, encoding = "utf-8")[:-1]
+
+            autoencoder.save('autoencoder_{}.h5'.format(new_class))
+
+            print("    # Agnostic class is \"{}\" : {}".format(new_class, input_data_agnostic_set.shape[0]))
+            print("    # Agnostic class is all : {}".format(input_data_aware_set.shape[0]))
+            print("    ## Training : {} (80%)".format(input_data_train_set.shape[0]))
+            print("    ## Testing : {} (20%)".format(input_data_test_set.shape[0]))
+
+            avg_train_aware_loss = autoencoder.evaluate(input_data_train_set, input_data_train_set, verbose=0)
+            print("    Average Loss of Training Aware Data = {}".format(avg_train_aware_loss))
+
+            avg_test_aware_loss = autoencoder.evaluate(input_data_test_set, input_data_test_set, verbose=0)
+            print("    Average Loss of Testing Aware Data = {}".format(avg_test_aware_loss))
+
+            avg_aware_loss = autoencoder.evaluate(input_data_aware_set, input_data_aware_set, verbose=0)
+            print("    Average Loss of Aware Data = {}".format(avg_aware_loss))
+
+            avg_agnostic_loss = autoencoder.evaluate(input_data_agnostic_set, input_data_agnostic_set, verbose=0)
+            print("    Average Loss of Agnostic Data = {}".format(avg_agnostic_loss))
+            print("---------------------------------------------------------")
+
+            writer.writerow([new_class, 
+                             input_data_agnostic_set.shape[0],
+                             input_data_aware_set.shape[0],
+                             input_data_train_set.shape[0],
+                             input_data_test_set.shape[0],
+                             avg_train_aware_loss,
+                             avg_test_aware_loss,
+                             avg_aware_loss,
+                             avg_agnostic_loss])
--- AutoEncoder_test.py
+++ AutoEncoder_test.py
@@ -20,33 +20,37 @@ if __name__ == '__main__':
 
     # parameter
     DATA_LENGTH = data.shape[0]
-    NEW_CLASS = b'smurf.'
+    NEW_CLASS = b'neptune.'
     CODES = 5
     THRESHOLD = 26
     # parameter
 
+    input_data_agnostic_set = list()
+    input_data_aware_set = list()
     input_data_test_set = list()
     input_data_train_set = list()
-
-    for index, each in enumerate(labels):
+    
+    for index, each in enumerate(labels[:DATA_LENGTH]):
         if each == NEW_CLASS:
-            input_data_test_set.append(data[index])
+            input_data_agnostic_set.append(data[index])
         else:
-            input_data_train_set.append(data[index])
+            input_data_aware_set.append(data[index])
 
-    input_data_test_set = np.array(input_data_test_set)
-    input_data_train_set = np.array(input_data_train_set)
+    input_data_agnostic_set = np.array(input_data_agnostic_set)
+    input_data_test_set = np.array(input_data_aware_set[int(len(input_data_aware_set)*4/5):])
+    input_data_train_set = np.array(input_data_aware_set[:int(len(input_data_aware_set)*4/5)])
+    input_data_aware_set = np.array(input_data_aware_set)
 
     autoencoder = load_model('autoencoder.h5')
 
     autoencoder.summary()
-
+    '''
     max_loss = None
     count = 0
     fn_label = [0 for each in label_list]
-    for index in range(input_data_train_set.shape[0]):
+    for index in range(input_data_test_set.shape[0]):
         try:
-            loss = autoencoder.evaluate(input_data_train_set[index:index + 1], input_data_train_set[index:index + 1],
+            loss = autoencoder.evaluate(input_data_test_set[index:index + 1], input_data_test_set[index:index + 1],
                                            verbose=0)
             if not max_loss or max_loss < loss:
                 max_loss = loss
@@ -55,26 +59,39 @@ if __name__ == '__main__':
                 count += 1
         except ValueError:
             print(index)
-
+    
     print("False Postive = {}".format(count))
     for index, each in enumerate(fn_label):
         print('{Class}: {number}'.format(Class=label_list[index], number=each))
-
-    print("Max Loss of Training Data = {}".format(max_loss))
+    
+    print("Max Loss of Aware Data = {}".format(max_loss))
 
     min_loss = None
-    for index in range(input_data_test_set.shape[0]):
+    for index in range(input_data_agnostic_set.shape[0]):
         try:
-            loss = autoencoder.evaluate(input_data_test_set[index:index + 1], input_data_test_set[index:index + 1],
+            loss = autoencoder.evaluate(input_data_agnostic_set[index:index + 1], input_data_agnostic_set[index:index + 1],
                                            verbose=0)
             if not min_loss or min_loss > loss:
                 min_loss = loss
         except ValueError:
             print(index)
     
-    print("Min Loss of Testing Data = {}".format(min_loss))
-    
+    print("Min Loss of Agnostic Data = {}".format(min_loss))
+    '''
+    print("    # Agnostic class is \"{}\" : {}".format(str(NEW_CLASS, encoding = "utf-8"),
+                                                   input_data_agnostic_set.shape[0]))
+    print("    # Agnostic class is all : {}".format(input_data_aware_set.shape[0]))
+    print("    ## Training : {} (80%)".format(input_data_train_set.shape[0]))
+    print("    ## Testing : {} (20%)".format(input_data_test_set.shape[0]))
+
     loss = autoencoder.evaluate(input_data_train_set, input_data_train_set, verbose=0)
-    print("Average Loss of Training Data = {}".format(loss))
+    print("    Average Loss of Training Aware Data = {}".format(loss))
+
     loss = autoencoder.evaluate(input_data_test_set, input_data_test_set, verbose=0)
-    print("Average Loss of Testing Data = {}".format(loss))
+    print("    Average Loss of Testing Aware Data = {}".format(loss))
+
+    loss = autoencoder.evaluate(input_data_aware_set, input_data_aware_set, verbose=0)
+    print("    Average Loss of Aware Data = {}".format(loss))
+
+    loss = autoencoder.evaluate(input_data_agnostic_set, input_data_agnostic_set, verbose=0)
+    print("    Average Loss of Agnostic Data = {}".format(loss))
